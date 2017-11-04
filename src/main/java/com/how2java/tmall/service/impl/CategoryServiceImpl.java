ac18e554c1a985aeab1f22de11f061a9ccb0cc82
leiziqin <ziqinlei@gmail.com>
2017-11-04 23:09:32 +0800

    update 分类管理页面 支持分页

--- src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.how2java.tmall.service.impl;
 import com.how2java.tmall.mapper.CategoryMapper;
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,12 @@ public class CategoryServiceImpl implements CategoryService {
     private CategoryMapper categoryMapper;
 
     @Override
-    public List<Category> list() {
-        return categoryMapper.list();
+    public List<Category> list(Page page) {
+        return categoryMapper.list(page);
+    }
+
+    @Override
+    public int total() {
+        return categoryMapper.total();
     }
 }
