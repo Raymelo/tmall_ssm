4884af5a137f60b742e34f665246e7e16e21adb9
leiziqin <ziqinlei@gmail.com>
2017-11-07 21:35:25 +0800

    add CategoryService

---
+++ src/main/java/com/how2java/tmall/service/impl/ProductServiceImpl.java
@@ -0,0 +1,63 @@
+package com.how2java.tmall.service.impl;
+
+import com.how2java.tmall.mapper.ProductMapper;
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.Product;
+import com.how2java.tmall.pojo.ProductExample;
+import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductMapper productMapper;
+    @Autowired
+    private CategoryService categoryService;
+
+    @Override
+    public void add(Product product) {
+        productMapper.insert(product);
+    }
+
+    @Override
+    public void delete(int id) {
+        productMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(Product product) {
+        productMapper.updateByPrimaryKeySelective(product);
+    }
+
+    @Override
+    public Product get(int id) {
+        Product product = productMapper.selectByPrimaryKey(id);
+        setCategory(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> list(int cid) {
+        ProductExample productExample = new ProductExample();
+        productExample.createCriteria().andCidEqualTo(cid);
+        productExample.setOrderByClause("id desc");
+        List<Product> products = productMapper.selectByExample(productExample);
+        setCategory(products);
+        return products;
+    }
+
+    private void setCategory(List<Product> products) {
+        products.forEach(this::setCategory);
+    }
+
+    private void setCategory(Product product) {
+        int cid = product.getCid();
+        Category category = categoryService.get(cid);
+        product.setCategory(category);
+    }
+}
